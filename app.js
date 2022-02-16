5dfa744cff1187c67c00d6f60dd393955053b6eb
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-16 16:07:26 +0600

    iphone update

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('I am in a separate file')
+function updateiPhone(isIncresing) {
+    const iphoneInput = document.getElementById('iphone_number');
+
+    let iphoneNumber = iphoneInput.value;
+    if (isIncresing) {
+        iphoneNumber = parseInt(iphoneInput.value) + 1;
+    }
+    else if (iphoneNumber > 0) {
+        iphoneNumber = parseInt(iphoneInput.value) - 1;
+    }
+    iphoneInput.value = iphoneNumber
+
+    const iPhoneTotal = document.getElementById('iphone_total');
+    iPhoneTotal.innerText = iphoneNumber * 1219;
+}
+
+document.getElementById('iphone_increse').addEventListener('click', function () {
+    updateiPhone(true);
+
+})
+document.getElementById('iphone_decrese').addEventListener('click', function () {
+    updateiPhone(false);
+
+})
